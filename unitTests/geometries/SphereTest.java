cea64811bf8ff0da8d0e7b490d3a076743d56ebd
Saritik <saritik16@gmail.com>
2023-04-10 20:56:21 +0300

    starting the test functions

---
+++ unitTests/geometries/SphereTest.java
@@ -0,0 +1,22 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SphereTest {
+
+    @Test
+    public void testConstructor(){
+
+    }
+
+    @Test
+    void testGetCenter() {
+
+    }
+
+    @Test
+    void testGetNormal() {
+    }
+}
---
+++ unitTests/geometries/TubeTest.java
@@ -0,0 +1,17 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TubeTest {
+    @Test
+    public void testConstructor(){
+        
+    }
+
+    @Test
+    void testGetNormal() {
+
+    }
+}
